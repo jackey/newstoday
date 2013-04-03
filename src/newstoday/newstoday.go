2eec7f4145ba619c7e699c4e8bbcae12dedeac5d
jackey <jziwenchen@gmail.com>
2013-04-03 14:47:20 +0800

    basic web server

--- src/newstoday/newstoday.go
+++ src/newstoday/newstoday.go
@@ -1,19 +1,48 @@
 package main
 
 import (
-	"beego"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
 )
 
-type MainController struct {
-	beego.Controller
+type Page struct {
+	title string
+	body  []byte
 }
 
-func (this *MainController) Get() {
-	this.Ctx.WriteString("hello world")
+func (p *Page) open(title string) *Page {
+	filename := title + ".html"
+	page := &Page{}
+	page.body, _ = ioutil.ReadFile(filename)
+	page.title = title
+	return page
+}
+
+func (p *Page) save(page Page) error {
+	filename := page.title + ".html"
+	return ioutil.WriteFile(filename, page.body, 0600)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	page := Page{}
+	opened_page := page.open("index")
+	fmt.Fprintf(w, "%s", opened_page.body)
+	//fmt.Fprintf(w, "Hi I like %s and %s", r.URL.Path[1:], r.URL.RawQuery[0:])
 }
 
 func main() {
-	beego.RegisterController("/", &MainController{})
-	//beego.HttpPort = 8080 // default
-	beego.Run()
+	// Add signal handler
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			fmt.Println("%i", sig)
+			os.Exit(1)
+		}
+	}()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
--- test/test.go
+++ test/test.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type Meter struct {
-	x, y float64
+type Method struct {
+	x, y int
+}
+
+func (m *Method) plus() int {
+	return m.x + m.y
+}
+
+func (m *Method) showme() string {
+	return "I am Method"
+}
+
+func (m *Method) tostring() string {
+	return string(m.x)
+}
+
+var (
+	a = Method{10, 20}
+)
+
+var (
+	b = []int{1, 2, 3, 4}
+)
+
+func main() {
+	fmt.Println(b)
 }
