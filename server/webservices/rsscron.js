2f8b0046a02ad4de2e577a9991e203e80d0b1181
jackey <jziwenchen@gmail.com>
2013-08-12 10:25:39 +0800

    remove duplicated code

--- server/webservices/rsscron.js
+++ server/webservices/rsscron.js
@@ -35,35 +35,3 @@ var readlinks = new Readlinks();
 
 (readlinks).each().execute();
 
-function Readlinks () {
-	this._links = require("./links");
-	this._pers = [];
-}
-
-Readlinks.prototype.each = function (cb) {
-	var self = this;
-	_.each(this._links, function (link, name) {
-		try {
-			var per = require("./provider/" + name);
-			per.setup(name, link)
-			self._pers.push(per);
-		}
-		catch(e) {
-			//
-			console.log(e);
-		}
-	})
-	return this;
-}
-
-Readlinks.prototype.execute = function () {
-	_.each(this._pers, function (provider) {
-		provider.execute();
-	})
-	return this;
-}
-
-// Do it
-var readlinks = new Readlinks();
-
-(readlinks).each().execute();
